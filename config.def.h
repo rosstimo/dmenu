4fe0dc3c843280eaec7e450d1721f379fee4ec9c
rosstimo <rosstimo@isu.edu>
2022-03-14 21:39:15 -0600

    add pywal

--- config.def.h
+++ config.def.h
@@ -2,19 +2,24 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=12"
+	"monospace:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+
+//get theme from wal
+#include "/home/tim/.cache/wal/colors-wal-dmenu.h"
+//static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+  //SchemeNorm] = { "#bbbbbb", "#222222" },
+	//[SchemeSel] = { "#eeeeee", "#005577" },
+	//[SchemeOut] = { "#000000", "#00ffff" },
+//};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 5;
 
 /*
  * Characters not considered part of a word while deleting words
